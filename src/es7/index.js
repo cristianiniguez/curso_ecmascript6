4b1a371cb37c192ac3ada36fefa97742c91f7263
cristianiniguez <felizpc1@gmail.com>
2020-08-07 14:43:01 -0400

    ¿Qué se implementó en ES7?

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 7, 8, 9];
+
+if (numbers.includes(0)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra');
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
