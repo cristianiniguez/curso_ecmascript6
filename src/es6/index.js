f0da524e10ff3adfd4df410bc76119a7a8a76f6b
cristianiniguez <felizpc1@gmail.com>
2020-08-06 20:48:34 -0400

    Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -95,7 +95,7 @@ const square = num => num * num;
 
 const helloPromise = () => {
     return new Promise((resolve, reject) => {
-        if(false) {
+        if (false) {
             resolve('Me resolví :)');
         } else {
             reject('Me rechazé :(');
@@ -106,3 +106,36 @@ const helloPromise = () => {
 helloPromise()
     .then(response => console.log(response))
     .catch(error => console.error(error))
+
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import { hello } from './module';
+
+hello();
+
+function* helloWorld() {
+    if(true) {
+        yield 'Hello, ';
+    }
+    if(true) {
+        yield 'World';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
