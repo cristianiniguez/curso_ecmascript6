95fdbeffe9fd10285a2b84d310f8b9312c83eefd
cristianiniguez <felizpc1@gmail.com>
2020-08-07 16:48:36 -0400

    ¿Qué se implementó en ES10?

---
+++ src/es10/index.js
@@ -0,0 +1,16 @@
+let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
+
+console.log(array.flat(2))
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(array.flatMap(value => [value, value * 2]));
+
+let hello = '    hello world'
+
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello = 'hello world      '
+console.log(hello);
+console.log(hello.trimEnd());
