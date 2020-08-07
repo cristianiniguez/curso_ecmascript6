aaafe330dfc13ab81632f1ee17e0942ee0cac115
cristianiniguez <felizpc1@gmail.com>
2020-08-07 15:57:57 -0400

    Async/Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -26,3 +26,31 @@ console.log('food'.padEnd(12, ' -------'));
 const obj = {
     name: 'cristian',
 }
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        false
+            ? setTimeout(() => {
+                resolve('Hello World')
+            }, 3000)
+            : reject(new Error('Test'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
