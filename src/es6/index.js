4a54d68b6483116eba2ab5ad041981a5ad4b44c4
cristianiniguez <felizpc1@gmail.com>
2020-08-06 20:21:32 -0400

    Arrow Functions, Promesas y Parámetros en Objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,7 +21,7 @@ let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
 let lorem = 'Lorem ipsum dolor sit amet,\n'
-+ 'consectetur adipiscing elit.';
+    + 'consectetur adipiscing elit.';
 
 //es6
 let lorem2 = `Lorem ipsum dolor sit amet,
@@ -63,3 +63,46 @@ console.log(globalLet);
 
 const a = 'b';
 a = 'a';
+
+let name = 'cristian';
+let age = 23;
+//es5
+let obj = { name: name, age: age };
+//es6
+let obj2 = { name, age };
+console.log(obj2);
+
+const names = [
+    { name: 'carlos', age: '22' },
+    { name: 'mary', age: '23' },
+]
+
+let listOfNames = names.map(function (item) {
+    console.log(item.name);
+})
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {
+    // ...
+}
+
+const listOfNames4 = name => {
+    // ...
+}
+
+const square = num => num * num;
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if(false) {
+            resolve('Me resolví :)');
+        } else {
+            reject('Me rechazé :(');
+        }
+    })
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .catch(error => console.error(error))
